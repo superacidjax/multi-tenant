819c181a128bbe2b8b7c052b9ab0d3075c117871
Brian Dear <superacidjax@me.com>
2013-05-27 14:28:33 -0400

    adds subdomain routing and constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
+require 'subscribem/constraints/subdomain_required'
 Subscribem::Engine.routes.draw do
+  constraints(Subscribem::Constraints::SubdomainRequired) do
+    scope module: "account" do
+      root to: "dashboard#index"
+      get '/sign_in', to: "sessions#new"
+      post '/sign_in', to: "sessions#create", as: :sessions
+    end
+  end
+
   root to: "dashboard#index"
 
   get '/sign_up', to: "accounts#new", as: :sign_up
