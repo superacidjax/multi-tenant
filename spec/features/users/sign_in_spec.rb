b8af9c12ad6da46ab0a415796a1a5bf94f944456
Brian Dear <superacidjax@me.com>
2013-05-27 14:41:34 -0400

    handles invalid login credentials

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -8,6 +8,7 @@ describe 'Users' do
   let(:root_url) { "http://#{account.subdomain}.example.com/" }
 
   within_account_subdomain do
+
     it "signs in as an account owner successfully" do
       visit subscribem.root_url(subdomain: account.subdomain)
       page.current_url.should == sign_in_url
@@ -16,6 +17,28 @@ describe 'Users' do
       click_button "Sign in"
       page.should have_content("You are now signed in.")
       page.current_url.should == root_url
+    end
+
+    it "attempts sign in with an invalid password and fails" do
+      visit subscribem.root_url(subdomain: account.subdomain)
+      page.current_url.should == sign_in_url
+      page.should have_content("Please sign in.")
+      fill_in "Email", with: account.owner.email
+      fill_in "Password", with: "drowssap"
+      click_button "Sign in"
+      page.should have_content("Invalid email or password.")
+      page.current_url.should == sign_in_url
+    end
+
+    it "attempts sign in with an invalid email address and fails" do
+      visit subscribem.root_url(subdomain: account.subdomain)
+      page.current_url.should == sign_in_url
+      page.should have_content("Please sign in.")
+      fill_in "Email", with: "foo@example.com"
+      fill_in "Password", with: "password"
+      click_button "Sign in"
+      page.should have_content("Invalid email or password.")
+      page.current_url.should == sign_in_url
     end
   end
 end
