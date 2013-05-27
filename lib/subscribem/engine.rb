b828ea2642c146ea96ad89df21a3fc62085da4eb
Brian Dear <superacidjax@me.com>
2013-05-27 18:00:29 -0400

    allows Warden use across the app and engine

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -2,8 +2,13 @@ require 'warden'
 module Subscribem
   class Engine < ::Rails::Engine
     isolate_namespace Subscribem
-    config.middleware.use Warden::Manager do |manager|
-      manager.default_strategies :password
+
+    config.to_prepare do
+      root = Subscribem::Engine.root
+      extenders_path = root + "app/extenders/**/*.rb"
+      Dir.glob(extenders_path) do |file|
+        Rails.configuration.cache_classes ? require(file) : load(file)
+      end
     end
   end
 end
